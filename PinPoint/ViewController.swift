e2adc9bc199742305f3a75c5454c512d7aa5d575
elmysf <elmysufiandy09@gmail.com>
2021-10-15 17:43:00 +0700

    Initial Commit

---
+++ PinPoint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PinPoint
+//
+//  Created by Sufiandy Elmy on 15/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
